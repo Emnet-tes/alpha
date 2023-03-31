dcaafaa55392b7151a15549ffcafb45fd4ece3fd
Emnet-tes <127366827+Emnet-tes@users.noreply.github.com>
2023-03-31 13:56:28 +0300

    modified alpha

--- main.cpp
+++ main.cpp
@@ -4,33 +4,42 @@ using namespace std;
 
 int main()
 {
-int n=1;
+int n;
 cout<<"\t\t__________PRINT NUMBER IN PATTERN__________\t\t\n";
 cout<<"enter number of rows: ";
 cin>>n;
 cout<<endl;
-for(int i=n;i>=1;i--)
+if(n>0)
 {
+ for(int i=n;i>=1;i--)
+ {
   for(int j=n-i;j>0;j--)
   cout<<"  ";
   for(int k=0;k<=i;k++)
   cout<<k<<" ";
   for(int m=i-1;m>=0;m--)
-  {
+    {
       cout<<m<<" ";
-      }
+    }
   cout<<endl;
-}
+ }
 
-for(int i=0;i<=n;i++)
-{
+ for(int i=0;i<=n;i++)
+ {
     for(int x=n-i;x>0;x--)
     cout<<"  ";
     for(int y=0;y<=i;y++)
     cout<<y<<" ";
     for(int z=i-1;z>=0;z--)
-    {cout<<z<<" ";}
+    {
+        cout<<z<<" ";
+    }
     cout<<endl;
-}
+ }
+}else
+ { 
+   cout<<"\t\t\t\tinvalid input"; 
+ }
+
     return 0;
 }
